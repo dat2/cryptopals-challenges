a900c7f27be69d5233f51817abc81c057215d32a
Nicholas Dujay <ndujay05@ca.ibm.com>
2016-03-05 16:35:04 -0500

    challenges 1, 2, 3 for cryptopals

---
+++ src/mycrypto/hexToBase64.go
@@ -0,0 +1,188 @@
+package mycrypto
+
+import (
+  "fmt"
+  "math"
+  "strings"
+  "encoding/hex"
+)
+
+// TOOD error checking
+func HexToBase64(hexbytes []byte) (base64Bytes []byte) {
+
+  var fullstring string = ""
+
+  // convert the bytes into one very long string
+  for i := 0; i < len(hexbytes); i++ {
+    fullstring = fullstring + fmt.Sprintf("%08b", hexbytes[i])
+  }
+
+  // every six characters make a new byte
+  var index int
+  for base := 0; base < len(fullstring) / 6; base++ {
+    index = base * 6
+
+    // convert the six characters into their bit representation
+    bits := fullstring[index:index+6]
+    var bitsSum byte
+    for i := 0; i < len(bits); i++ {
+      bitsSum = bitsSum + (1 << byte(len(bits) - i - 1)) * byte(bits[i] - '0')
+    }
+
+    // add it to the result string
+    base64Bytes = append(base64Bytes, bitsSum)
+  }
+
+  return
+}
+
+// TODO error checking
+func XOR(a, b []byte) (result []byte) {
+  for i := 0; i < len(a); i++ {
+    result = append(result, a[i] ^ b[i])
+  }
+
+  return
+}
+
+func englishScore(str string) (error float64) {
+
+  // taken from wikipedia
+  // https://en.wikipedia.org/wiki/Letter_frequency
+  englishFreq := map[byte]float64 {
+    'e': 0.12702,
+    't': 0.09056,
+    'a': 0.08167,
+    'o': 0.07507,
+    'i': 0.06966,
+    'n': 0.06749,
+    's': 0.06327,
+    'h': 0.06094,
+    'r': 0.05987,
+    'd': 0.04253,
+    'l': 0.04025,
+    'c': 0.02782,
+    'u': 0.02758,
+    'm': 0.02406,
+    'w': 0.02361,
+    'f': 0.02228,
+    'g': 0.02015,
+    'y': 0.01974,
+    'p': 0.01929,
+    'b': 0.01492,
+    'v': 0.00978,
+    'k': 0.00772,
+    'j': 0.00153,
+    'x': 0.00150,
+    'q': 0.00095,
+    'z': 0.00074,
+  }
+
+  total := float64(0)
+  strFreq := map[byte]float64 {
+    'e': 0,
+    't': 0,
+    'a': 0,
+    'o': 0,
+    'i': 0,
+    'n': 0,
+    's': 0,
+    'h': 0,
+    'r': 0,
+    'd': 0,
+    'l': 0,
+    'c': 0,
+    'u': 0,
+    'm': 0,
+    'w': 0,
+    'f': 0,
+    'g': 0,
+    'y': 0,
+    'p': 0,
+    'b': 0,
+    'v': 0,
+    'k': 0,
+    'j': 0,
+    'x': 0,
+    'q': 0,
+    'z': 0,
+  }
+
+  lower := strings.ToLower(str)
+  for i := 0; i < len(lower); i++ {
+    _, ok := strFreq[lower[i]]
+    if ok {
+      strFreq[lower[i]] = strFreq[lower[i]] + 1
+      total = total + 1
+    }
+  }
+
+  for k := range englishFreq {
+    error = error + math.Abs(englishFreq[k] - strFreq[k] / total)
+  }
+
+  return
+}
+
+func XORDecrypt(encrypted []byte) (result []byte) {
+
+  // from A-Z
+  minError := math.Inf(1)
+  for i := 65; i <= 90; i++ {
+    // make the key
+    key := make([]byte, len(encrypted));
+    for j := 0; j < len(encrypted); j++ {
+      key[j] = byte(i)
+    }
+
+    // decrypt the bytes
+    decryptedBytes := XOR(encrypted, key)
+    decrypted := string(decryptedBytes)
+    err := englishScore(decrypted)
+    if err < minError {
+      minError = err
+      result = decryptedBytes
+    }
+  }
+
+  return
+}
+
+/*
+UTIL FUNCTIONS
+ */
+func HexToBytes(input string) (hexbytes []byte) {
+  hexbytes,_ = hex.DecodeString(input)
+  return
+}
+
+func HexBytesToString(input []byte) (result string) {
+  result = hex.EncodeToString(input)
+  return
+}
+
+func Base64BytesToString(input []byte) (result string) {
+
+  result = ""
+  for i := 0; i < len(input); i++ {
+    if input[i] <= 25 {
+      result = result + string(input[i] + 'A')
+    } else if input[i] <= 51 {
+      result = result + string((input[i] - 26) + 'a')
+    } else if input[i] <= 61 {
+      result = result + string((input[i] - 52) + '0')
+    } else if input[i] == 62 {
+      result = result + "+"
+    } else {
+      result = result + "/"
+    }
+  }
+
+  // pad == at the end
+  // numPadChars := 3 - int(math.Mod(float64(len(input)), 3))
+  // for i := 0; i < numPadChars; i++ {
+    // result = result + "="
+  // }
+
+  return
+}
---
+++ src/set1/set1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fmt"
+  "mycrypto"
+)
+
+func main() {
+  // challenge 1
+  fmt.Println("Challenge 1")
+  fmt.Println("Expecting:","SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+  bytes := mycrypto.HexToBytes("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+  base64Bytes := mycrypto.HexToBase64(bytes)
+  fmt.Println("Actual:   ",mycrypto.Base64BytesToString(base64Bytes))
+  fmt.Println()
+
+  // challenge 2
+  fmt.Println("Challenge 2")
+  fmt.Println("Expecting:","746865206b696420646f6e277420706c6179")
+  a := mycrypto.HexToBytes("1c0111001f010100061a024b53535009181c")
+  b := mycrypto.HexToBytes("686974207468652062756c6c277320657965")
+  c := mycrypto.XOR(a, b)
+  fmt.Println("Actual:   ", mycrypto.HexBytesToString(c))
+  fmt.Println()
+
+  // challenge 3
+  fmt.Println("Challenge 3")
+  fmt.Println("Encrypted String:", "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+  encrypted := mycrypto.HexToBytes("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+  result := mycrypto.XORDecrypt(encrypted)
+  fmt.Println("Decrypted Result:", string(result))
+  fmt.Println()
+}
