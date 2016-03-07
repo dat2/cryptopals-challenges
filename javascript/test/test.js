3bc21c332245d0e2aba61237fc55c73c73ce61ba
Nicholas Dujay <ndujay05@ca.ibm.com>
2016-03-06 22:33:26 -0500

    added tests, split stuff up

---
+++ javascript/test/test.js
@@ -0,0 +1,85 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const utils = require('../utils');
+const set1 = require('../set1');
+
+describe('set1', function (){
+  describe('challenge1', function () {
+    it('should pass the test case given', function () {
+      const input = utils.hexStringToBuffer('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d');
+      expect(set1.hexToBase64(input).toString()).to.equal('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t');
+    });
+  });
+
+  describe('challenge2', function() {
+    it('should pass the test case given', function() {
+      const a = utils.hexStringToBuffer('1c0111001f010100061a024b53535009181c');
+      const b = utils.hexStringToBuffer('686974207468652062756c6c277320657965');
+      const result = utils.bufferToHexString(set1.fixedXor(a, b));
+
+      expect(result).to.be.equal('746865206b696420646f6e277420706c6179');
+    })
+  });
+
+  describe('challenge3', function() {
+    it('should pass the test case given', function() {
+      const encrypted = utils.hexStringToBuffer('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736');
+      const decrypted = set1.decryptXor(encrypted).buffer;
+      expect(decrypted.toString()).to.be.equal('Cooking MC\'s like a pound of bacon');
+    });
+  });
+
+  describe('challenge4', function() {
+    it('should pass the test case given', function() {
+      const p = set1.detectFixedXor('4.txt').then(b => b.toString());
+      return expect(p).to.eventually.equal('Now that the party is jumping\n');
+    });
+  });
+
+  describe('challenge5', function() {
+    it('should pass the test case given', function() {
+      const inputString = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+      const encrypted = set1.encryptRepeatingKeyXor('ICE', inputString);
+      const expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f';
+      expect(utils.bufferToHexString(encrypted)).to.be.equal(expected);
+    });
+  });
+
+  describe('challenge6', function() {
+    it('should calculate hamming distance correctly', function() {
+      const a = new Buffer('this is a test');
+      const b = new Buffer('wokka wokka!!!');
+
+      expect(utils.hammingDistance(a, b)).to.be.equal(37);
+    });
+
+    it('should convert base64 strings to a buffer', function() {
+      const a = new Buffer('TWFu');
+      const result = new Buffer('Man');
+      expect(utils.base64ToBuffer(a)).to.deep.equal(result);
+
+      const b = new Buffer('YW55IGNhcm5hbCBwbGVhc3VyZS4=');
+      const result2 = new Buffer('any carnal pleasure.');
+      expect(utils.base64ToBuffer(b)).to.deep.equal(result2);
+
+      const c = new Buffer('YW55IGNhcm5hbCBwbGVhc3VyZQ==');
+      const result3 = new Buffer('any carnal pleasure');
+      expect(utils.base64ToBuffer(c)).to.deep.equal(result3);
+    });
+
+    it('should transpose buffers correctly', function() {
+      const bufs = [new Buffer([1,2,3]), new Buffer([4,5,6]), new Buffer([7,8,9])];
+      const transpose = [new Buffer([1,4,7]), new Buffer([2,5,8]), new Buffer([3,6,9])];
+
+      expect(Buffer.concat(utils.transpose(bufs), 9)).to.deep.equal(Buffer.concat(transpose, 9));
+    });
+
+    it('should break repeating-key xor', function() {
+      this.timeout(15000);
+      return expect(set1.breakRepeatingKeyXor('6.txt').then(b => b.toString())).to.eventually.contain("I\'m back and I\'m ringin\' the bell")
+    });
+  })
+});
---
+++ javascript/utils.js
@@ -0,0 +1,222 @@
+'use strict';
+const fs = require('fs');
+const readline = require('readline');
+
+// https://en.wikipedia.org/wiki/Letter_frequency
+const englishLetterFrequency = {
+  e: 0.12702,
+  t: 0.09056,
+  a: 0.08167,
+  o: 0.07507,
+  i: 0.06966,
+  n: 0.06749,
+  s: 0.06327,
+  h: 0.06094,
+  r: 0.05987,
+  d: 0.04253,
+  l: 0.04025,
+  c: 0.02782,
+  u: 0.02758,
+  m: 0.02406,
+  w: 0.02361,
+  f: 0.02228,
+  g: 0.02015,
+  y: 0.01974,
+  p: 0.01929,
+  b: 0.01492,
+  v: 0.00978,
+  k: 0.00772,
+  j: 0.00153,
+  x: 0.00150,
+  q: 0.00095,
+  z: 0.00074,
+};
+
+const stringLetterFrequency = string => {
+  const counter = Object.assign({}, englishLetterFrequency);
+  for(let key in counter) {
+    counter[key] = 0;
+  }
+  let total = 0;
+  const count = Array.prototype.reduce.call(string, (acc, letter) => {
+    if(letter in acc) {
+      acc[letter]++;
+      total++;
+    }
+    return acc;
+  }, counter);
+  for(let key in counter) {
+    counter[key] /= total;
+  }
+
+  return counter;
+};
+
+const stringError = string => {
+  const freq = stringLetterFrequency(string);
+  return Object.keys(freq).reduce((acc, letter) => acc + Math.abs(freq[letter] - englishLetterFrequency[letter]) , 0);
+};
+
+const makeBitString = str => Array.prototype.map.call(str, n => n.toString(2))
+  .map(padBinary)
+  .join('');
+
+const hammingDistance = (a, b) => {
+  let bitStringA = makeBitString(a);
+  let bitStringB = makeBitString(b);
+  const diff = Math.abs(bitStringA.length - bitStringB.length);
+
+  return Array.prototype.reduce.call(bitStringA, (acc, char, i) => acc + (char !== bitStringB[i] ? 1 : 0), 0);
+};
+
+const padBinary = num => ('00000000' + num).slice(-8);
+const padBase64 = num => ('000000' + num).slice(-6);
+
+const slash = '/'.charCodeAt(0);
+const plus = '+'.charCodeAt(0);
+const equal = '='.charCodeAt(0);
+const zero = '0'.charCodeAt(0);
+const nine = '9'.charCodeAt(0);
+const a = 'a'.charCodeAt(0);
+const z = 'z'.charCodeAt(0);
+const A = 'A'.charCodeAt(0);
+const Z = 'Z'.charCodeAt(0);
+
+const hexCharToNum = byte => {
+  let rtn = 0;
+  if(byte >= a) {
+    rtn = byte - a + 10;
+  } else {
+    rtn = byte - zero;
+  }
+
+  return rtn;
+};
+
+const hexStringToBuffer = hexString => new Buffer(hexString, 'hex');
+const bufferToHexString = buffer => buffer.toString('hex');
+const stringToBuffer = string => new Buffer(string);
+
+const numToBase64 = num => {
+  if(num === 63) {
+    return '/'
+  } else if(num === 62) {
+    return '+';
+  } else if (num >= 52) {
+    return String.fromCharCode(num + zero - 52);
+  } else if (num >= 26) {
+    return String.fromCharCode(num + a - 26);
+  } else {
+    return String.fromCharCode(num + A);
+  }
+};
+
+const base64ToNum = code => {
+  if(code === slash) {
+    return 63;
+  } else if(code == plus) {
+    return 62;
+  } else if(code <= nine && code >= zero) {
+    return (code - zero) + 52;
+  } else if(code <= z && code >= a) {
+    return (code - a) + 26;
+  } else if(code <= Z && code >= A) {
+    return (code - A);
+  } else if (code === equal) {
+    return 0;
+  } else {
+  }
+};
+
+const readLines = filename => new Promise((resolve, reject) => {
+  const fileStream = fs.createReadStream(filename);
+  const lineReader = readline.createInterface({
+    input: fileStream
+  });
+  const lines = [];
+  lineReader.on('line', lines.push.bind(lines));
+  lineReader.on('close', () => resolve(lines));
+
+  lineReader.on('error', reject);
+});
+
+const readFile = filename => new Promise((resolve, reject) => {
+  fs.readFile(filename, (err, buffer) => {
+    if(err) { reject(err); }
+    else { resolve(buffer); }
+  });
+});
+
+const writeFile = (filename, contents) => new Promise((resolve, reject) => {
+  fs.writeFile(filename, contents, err => {
+    if(err) { return reject(err); }
+    resolve();
+  })
+});
+
+const base64ToBuffer = buffer => {
+  const base64Nums = Array.prototype.map.call(buffer, base64ToNum);
+
+  const binaryString = base64Nums
+    .map(s => s.toString(2))
+    .map(padBase64)
+    .join('');
+
+  const nums = [];
+  for(let i = 0; i < binaryString.length / 8; i++) {
+    nums.push(parseInt(binaryString.slice(i*8, i*8 + 8), 2));
+  }
+
+  // remove final == and = padding numbers
+  if(nums[nums.length - 1] === 0) {
+    nums.pop();
+  }
+  if(nums[nums.length - 1] === 0) {
+    nums.pop();
+  }
+
+  return new Buffer(nums);
+};
+
+const breakIntoBlocks = (buffer, blockSize) => {
+  const result = [];
+
+  for(let i = 0; i < buffer.length / blockSize; i++) {
+    result.push(buffer.slice(i * blockSize, i * blockSize + blockSize));
+  }
+
+  const last = result.pop();
+  result.push(new Buffer(blockSize).fill(0));
+  last.copy(result[result.length - 1]);
+
+  return result;
+};
+
+const transpose = blocks => {
+  const keySize = blocks[0].length;
+
+  const result = [];
+  for(let i = 0; i < keySize; i++) {
+    result.push(new Buffer(blocks.length).fill(0));
+    for(let j = 0; j < blocks.length; j++) {
+      result[i][j] = blocks[j][i];
+    }
+  }
+
+  return result;
+};
+
+module.exports = {
+  hexStringToBuffer,
+  padBinary,
+  bufferToHexString,
+  stringError,
+  numToBase64,
+  readLines,
+  readFile,
+  stringToBuffer,
+  hammingDistance,
+  base64ToBuffer,
+  transpose,
+  breakIntoBlocks
+};
